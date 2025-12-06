fa21dd667cdb474c70b282932825305c190a375d
Abdullah200305 <112935137+Abdullah200305@users.noreply.github.com>
2025-12-06 21:34:53 +0100

    user and some stuff

--- src/main/java/team/databasenmysql/model/IBooksDbMockImpl.java
+++ src/main/java/team/databasenmysql/model/IBooksDbMockImpl.java
@@ -120,8 +120,26 @@ public class IBooksDbMockImpl implements IBooksDb {
     }
 
 
+    @Override
+    public User CheckUser(String User,String password){
+        User result = null;
+        String sql = String.format("SELECT FULL_NAME,PASSKODE,SSN\n" +
+                "FROM t_customer\n" +
+                "WHERE FULL_NAME = '%s'\n" +
+                "  AND PASSKODE = '%s';\n",User,password);
+        try {
+            Statement statement = conn.createStatement();
+            ResultSet rs = statement.executeQuery(sql);
+            while (rs.next()){
+                result = new User(rs.getString(3),rs.getString(2),rs.getString(3));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
 
+        return null;
+    };
 
 
 

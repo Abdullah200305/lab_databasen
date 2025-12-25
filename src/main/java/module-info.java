627f2814e0d0e6af3d013daee86c5ccd406c4662
Abdullah200305 <112935137+Abdullah200305@users.noreply.github.com>
2025-12-25 12:46:53 +0100

    Jag har gjort action för connection och disconnection samt hämta infromation från databasen genom att skapa Db_book()

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,11 @@ module team.databasenmysql {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires org.mongodb.driver.sync.client;
+    requires org.mongodb.driver.core;
+    requires mysql.connector.j;
+    requires java.desktop;
+    requires org.mongodb.bson;
 
 
     opens team.databasenmysql to javafx.fxml;
--- src/main/java/team/databasenmysql/Main.java
+++ src/main/java/team/databasenmysql/Main.java
@@ -1,10 +1,20 @@
 package team.databasenmysql;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mysql.cj.xdevapi.Client;
+import com.mysql.cj.xdevapi.Session;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.bson.Document;
 import team.databasenmysql.model.*;
 import team.databasenmysql.view.BooksPane;
+
 import java.io.IOException;
 import java.sql.SQLException;
 
@@ -12,6 +22,19 @@ import java.sql.SQLException;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException, SQLException {
+
+        ///  twst for mondb
+
+        MongoClient mongoClient = MongoClients.create(
+                MongoClientSettings.builder().applyConnectionString(
+                        new ConnectionString(
+                                "mongodb://localhost:27017/")).build());
+
+        MongoDatabase database = mongoClient.getDatabase("mySchool");
+        MongoCollection<Document> collection = database.getCollection("mySchool");
+
+
+
         IBooksDb booksDb = new IBooksDbMockImpl();
        // model
         BooksPane booksPane = new BooksPane(booksDb);
